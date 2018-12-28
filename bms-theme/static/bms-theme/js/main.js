a19e9351a35a598e7b4d93c0584f1d0da7035f8d
Raghav Khandelwal <raghavthecyberdude@gmail.com>
2018-12-28 19:28:08 +0530

    Enabled ajax loading of content

--- bms-theme/static/bms-theme/js/main.js
+++ bms-theme/static/bms-theme/js/main.js
@@ -14,6 +14,23 @@ $(document).ready(function() {
 		$('a[aria-expanded=true]').attr('aria-expanded', 'false');
 	});
 	$("#bms-sidebar-header").height($("#bms-header").height());
+
+	$('.bms-tree .bms-tree-link').on('click', function(e){
+		var target = $(this).attr('href');
+		var parser = document.createElement('a');
+		parser.href = target;
+		$.ajax({
+			async: false,
+			url: target,
+			complete: function(responseHtml){
+				var newTitle = $(responseHtml).filter('title').text();
+				$('#bms-content').html( $(responseHtml.responseText).find('#bms-content').html() );
+				$('#bms-header-bottom').html( $(responseHtml.responseText).find('#bms-header-bottom').html() );
+				window.history.pushState(newTitle, newTitle, parser.pathname);
+			}
+		});
+		return false;
+	});
 });
 
 $(window).resize(function() {
